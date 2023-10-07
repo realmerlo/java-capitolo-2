742242fcbfc9faf7e6b11922c22d29552ce58cad
Riccardo Merli <riccardo.merli017@gmail.com>
2023-10-07 16:48:01 +0200

    es3 added

---
+++ src/es3/Car.java
@@ -0,0 +1,58 @@
+package es3;
+
+public class Car {
+
+    private int cc;
+    private String carPlate;
+    private String brand;
+    private String model;
+
+    public Car (int cc, String carPlate, String brand, String model){
+        this.cc = cc;
+        this.carPlate = carPlate;
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+
+    public String getCarPlate() {
+        return carPlate;
+    }
+
+    public void setCarPlate(String carPlate) {
+        this.carPlate = carPlate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "cc=" + cc +
+                ", carPlate='" + carPlate + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
--- src/es3/Main.java
+++ src/es3/Main.java
@@ -1,4 +1,16 @@
 package es3;
 
 public class Main {
+    public static void main(String[] args){
+        Car car = new Car (1100, "LS735PF", "Fiat", "Pandino4x4");
+
+        System.out.println("cc: " + car.getCc());
+        System.out.println("car plate: " + car.getCarPlate());
+        System.out.println("brand: " + car.getBrand());
+        System.out.println("model: " + car.getModel());
+
+        car.setModel("Punto");
+
+        System.out.println("new model is: " + car.getModel());
+    }
 }
